1783613301af9ba5f771b33dd42d0b612cfa401d
Alex Basystyi <alexbasisty@gmail.com>
2022-03-26 08:44:51 +0100

    metoda pokazu tekstu literami roznej wielkosci

--- MEGAK/hometasks/task1_26_03.js
+++ MEGAK/hometasks/task1_26_03.js
@@ -13,23 +13,39 @@
     Prosty kalkulator
 */
 
-const a = Number(prompt('Podaj liczbę a'));
-const b = Number(prompt('Podaj liczbę b'));
-const operator = prompt('Podaj operator działania');
-let result = 0;
+// const a = Number(prompt('Podaj liczbę a'));
+// const b = Number(prompt('Podaj liczbę b'));
+// const operator = prompt('Podaj operator działania');
+// let result = 0;
 
-if (operator === '-') {
-    result = a - b;
-    console.log(`Twoj wynik - to ${result}`);
-} else if (operator === '*') {
-    result = a * b;
-    console.log(`Twoj wynik - to ${result}`);
-} else if (operator === '/') {
-    result = a / b;
-    console.log(`Twoj wynik - to ${result}`);
-} else if (operator === '+') {
-    result = a + b;
-    console.log(`Twoj wynik - to ${result}`);
-} else {
-    console.log('Podałeś nieprawidłowe dane');
+// if (operator === '-') {
+//     result = a - b;
+//     console.log(`Twoj wynik - to ${result}`);
+// } else if (operator === '*') {
+//     result = a * b;
+//     console.log(`Twoj wynik - to ${result}`);
+// } else if (operator === '/') {
+//     result = a / b;
+//     console.log(`Twoj wynik - to ${result}`);
+// } else if (operator === '+') {
+//     result = a + b;
+//     console.log(`Twoj wynik - to ${result}`);
+// } else {
+//     console.log('Podałeś nieprawidłowe dane');
+// }
+
+/*
+ Metoda Show text
+*/
+
+const text = prompt('Napisz jakiś tekst')
+
+for (let i = 0; i < text.length; i++) {
+    if (i % 2 !== 0) {
+        const letter = text[i].toLowerCase();
+        console.log(letter);        
+    } else {
+        const letter = text[i].toUpperCase();
+        console.log(letter);  
+    }
 }
