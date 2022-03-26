e07b0b13a82159ba82071827c10e4f82db34d0e5
Alex Basystyi <alexbasisty@gmail.com>
2022-03-26 08:36:58 +0100

    prosty kalkulator

--- MEGAK/hometasks/task1_26_03.js
+++ MEGAK/hometasks/task1_26_03.js
@@ -1,10 +1,35 @@
 /*
     Prosta isnstrukcja warunkowa
 */
-const name = prompt('Jak się nazywasz?');
-const surname = prompt('Nazwisko, poproszę');
-const age = Number(prompt('Podaj swój wiek'));
+// const name = prompt('Jak się nazywasz?');
+// const surname = prompt('Nazwisko, poproszę');
+// const age = Number(prompt('Podaj swój wiek'));
 
-if (age >= 18) {
-    console.log(`${name} ${surname}`);
+// if (age >= 18) {
+//     console.log(`${name} ${surname}`);
+// }
+
+/* 
+    Prosty kalkulator
+*/
+
+const a = Number(prompt('Podaj liczbę a'));
+const b = Number(prompt('Podaj liczbę b'));
+const operator = prompt('Podaj operator działania');
+let result = 0;
+
+if (operator === '-') {
+    result = a - b;
+    console.log(`Twoj wynik - to ${result}`);
+} else if (operator === '*') {
+    result = a * b;
+    console.log(`Twoj wynik - to ${result}`);
+} else if (operator === '/') {
+    result = a / b;
+    console.log(`Twoj wynik - to ${result}`);
+} else if (operator === '+') {
+    result = a + b;
+    console.log(`Twoj wynik - to ${result}`);
+} else {
+    console.log('Podałeś nieprawidłowe dane');
 }
