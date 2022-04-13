a02310a7551e4adfa1555a0eb60846b04454c4d0
Alex Basystyi <alexbasisty@gmail.com>
2022-04-13 08:38:30 +0200

    some work

--- MEGAK/shopWithBusket/src/basket.js
+++ MEGAK/shopWithBusket/src/basket.js
@@ -34,18 +34,3 @@ class Product {
         this.price = newPrice;
     }
 }
-
-const shopBasket = new Basket();
-
-const oranges = new Product('Pomarańcze LUZ', 7.55);
-const cucumbers = new Product('Ogórek duży', 8.20);
-
-shopBasket.add(cucumbers);
-shopBasket.add(cucumbers);
-shopBasket.add(oranges);
-
-console.log(shopBasket.getTotalValue());
-shopBasket.remove(2);
-shopBasket.showBasket();
-
-console.log(shopBasket.getTotalValue());
