29f5721bbf2a78ac140e38143c72eecee037c30d
Alex Basystyi <alexbasisty@gmail.com>
2022-04-19 09:28:33 +0200

    product list from local storage

--- MEGAK/mini_pojects/shopWithBusket/src/admin-ui.js
+++ MEGAK/mini_pojects/shopWithBusket/src/admin-ui.js
@@ -3,30 +3,51 @@ const nameInput = document.querySelector('[name="product-name"]');
 const priceInput = document.querySelector('[name="product-price"]');
 const productsUL = document.querySelector('.products-list');
 
-const addProductToShop = event => {
-    event.preventDefault();
+const saveProductsToLocalStorage = (name, price) => {
+    const oldProductsList = JSON.parse(localStorage.getItem('shop-products')) ?? [];
+    oldProductsList.push({ name, price });
+    localStorage.setItem('shop-products', JSON.stringify(oldProductsList));
+};
 
-    const name = nameInput.value;
-    const price = Number(priceInput.value).toFixed(2);
-
+const addProductToShop = (name, price) => {
     const newLi = document.createElement('li');
-
+    
     const newStrong = document.createElement('strong');
     newStrong.innerText = name;
 
     const newPriceText = document.createTextNode(` - ${price} `);
-
+    
     const newButton = document.createElement('button');
     newButton.classList.add('btn-buy-product');
     newButton.dataset.name = name;
     newButton.dataset.price = String(price);
     newButton.innerText = 'Kup';
     newButton.addEventListener('click', addProductToBasket);
-
+    
     newLi.appendChild(newStrong);
     newLi.appendChild(newPriceText);
     newLi.appendChild(newButton);
     productsUL.appendChild(newLi);
+};
+
+
+const loadProductsFromLocalStorage = () => {
+    const productsList = JSON.parse(localStorage.getItem('shop-products')) ?? [];
+
+    for (const { name, price } of productsList) {
+        addProductToShop(name, price);
+    };
 }
 
-addProductForm.addEventListener('submit', addProductToShop);
+const handleAddProductFormSubmit = event => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const price = Number(priceInput.value).toFixed(2);
+
+    addProductToShop(name, price);
+    saveProductsToLocalStorage(name, price);
+}
+
+addProductForm.addEventListener('submit', handleAddProductFormSubmit);
+loadProductsFromLocalStorage();
