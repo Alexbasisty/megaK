2521451869a097756a879fb8a1dca37995ad61b1
Alex Basystyi <alexbasisty@gmail.com>
2022-05-11 19:55:01 +0200

    promises

--- MEGAK/index.js
+++ MEGAK/index.js
@@ -1,7 +1,36 @@
 const { readFile } = require("fs");
+const { promisify } = require("util");
+const { lookup, promises } = require("dns");
 
 console.log("Hello world");
 
-readFile("./index.js", "utf-8", (err,data) => {
+const readFilePromised = promisify(readFile);
+const lookupPromise = promisify(lookup);
+
+const host = 'google.com';
+
+lookupPromise(host)
+  .then(data => console.log(data.address));
+
+(async () => {
+  try {
+    const hostIP = await promises.lookup(host);
+
+    console.log(hostIP.address);
+  } catch (error) {
+    console.log('ooops', error);
+  }
+})();
+
+lookup(host, (err, data) => {
   console.log(data);
 });
+
+// (async () => {
+//   try {
+//     const data = await readFilePromised("./index.js", "utf-8")
+//     console.log(data);    
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
