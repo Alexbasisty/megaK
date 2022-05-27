eaf0a2c0cf1faf519f3b54000389d965239aaf55
Alex Basystyi <alexbasisty@gmail.com>
2022-05-27 08:33:44 +0200

    add encode

--- MEGAK/cipher.js
+++ MEGAK/cipher.js
@@ -1,5 +1,5 @@
 const { createHmac, pbkdf2 } = require('crypto');
-const { hash } = require('bcrypt');
+const { hash, compare } = require('bcrypt');
 
 const salt = 'Askjfa&)##()*# lkja  oei jwe))(* jfklas<L:AKFJLDFJWEOfj39439834823498pJKL:LJL;fdadl';
 const originalText = 'Tekst do zhaszowania';
@@ -19,8 +19,17 @@ pbkdf2(originalText, salt, 100000, 64, 'sha512', (err, derivedKey) => {
 
 // Third version - the safest
 
-hash(originalText, 10, (err, hashString) => {
+hash(originalText, 10, (err, hashString) => { // '10' here is an amount of ronds of making salt
     if (err) throw err;
 
     console.log(hashString);
+
+    compare(originalText, hashString, (error, res) => {
+        if (error) console.error(error);
+        if (res) {
+            console.log('Logged in!');
+        } else {
+            console.log('Nope!');
+        }
+    });
 });
