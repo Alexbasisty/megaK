d10e5634d25de69dd77a0df4bc15cc20f56eb43d
Alex Basystyi <alexbasisty@gmail.com>
2022-05-31 21:19:27 +0200

    decrypt/encrypt miniproj

---
+++ mini_projects/Decrypt_encrypt/cipher.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-shadow */
+const { promisify } = require('util');
+const scrypt = promisify(require('crypto').scrypt);
+const randomBytes = promisify(require('crypto').randomBytes);
+
+async function encryptText(text, password, salt) {
+    const algorithm = 'aes-192-cbc';
+    const key = await scrypt(password, salt, 24);
+    const iv = await randomBytes(16);
+
+    const cipher = createCipheriv(algorithm, key, iv);
+    let encrypted = cipher.update(text, 'utf-8', 'hex');
+    encrypted += cipher.final('hex');
+
+    return {
+        encrypted,
+        iv: iv.toString('hex'),
+    };
+}
+
+async function decryptText(text, password, salt, ivHex) {
+    const algorithm = 'aes-192-cbc';
+    const key = await scrypt(password, salt, 24);
+    const iv = Buffer.from(ivHex, 'hex');
+
+    const decipher = createDecipheriv(algorithm, key, iv);
+    let decrypted = decipher.update(text, 'hex', 'utf-8');
+    decrypted += decipher.final('utf-8');
+
+    return decrypted;
+}
+
+module.exports = {
+    encryptText,
+    decryptText,
+};
---
+++ mini_projects/Decrypt_encrypt/constants.js
@@ -0,0 +1,5 @@
+const SALT = 'asd sDFOJD FPOFJ$EO#*U#(#UO JIELKJLDF:LSDJ F#*(#OUEISJFLKDJSF:DJFO#(*#U jlkdjfa;l dfoao8';
+
+module.exports = {
+    SALT,
+};
