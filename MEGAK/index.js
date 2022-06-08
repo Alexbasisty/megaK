4b11a876b347edd97c9ba357b97beefc2180df56
Alex Basystyi <alexbasisty@gmail.com>
2022-06-08 08:52:31 +0200

    https requests and responses

--- MEGAK/index.js
+++ MEGAK/index.js
@@ -1,12 +1,16 @@
 const { createServer } = require('http');
+const { readFile } = require('fs').promises;
 
 const server = createServer();
 
 server
-    .on('request', (req, res) => {
+    .on('request', async (req, res) => {
+        const html = await readFile('./index.html', 'utf-8');
+
         res.writeHead(200, {
             'Content-Type': 'text/html',
+            'accept-language': 'uk-UA',
         });
-        res.end('<h1>Привіт, Україна</h1>');
+        res.end(html);
     })
     .listen(3000, 'localhost');
