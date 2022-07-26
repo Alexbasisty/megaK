f341bec7734f538b67132f4146c6672ef0dd2f9c
Alex Basystyi <alexbasisty@gmail.com>
2022-07-26 15:45:19 +0200

    express handlebars

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
 const { cookieRouter } = require('./routes/cookieRouter');
 
 const app = express();
@@ -8,7 +9,15 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 app.use('/cookie', cookieRouter);
+app.get('/hi', (req, res) => {
+    res.render('home');
+});
 
 app.listen(3000, 'localhost');
--- expressjs/routes/cookieRouter.js
+++ expressjs/routes/cookieRouter.js
@@ -4,11 +4,32 @@ const express = require('express');
 const cookieRouter = express.Router();
 
 cookieRouter
+    .post('/set', (req, res) => {
+        const { name } = req.body;
+        res
+            .cookie('name', name, {
+                maxAge: 1000 * 60 * 60 * 24 * 30,
+            })
+            .send(`
+            <!DOCTYPE html>
+                <body>
+                    <img src="/logo.bmp">
+                    <hr>
+                    <p>Zapisano imię</p>
+                </body>
+            </html>
+            `);
+    })
+    .get('/show', (req, res) => {
+        const { name } = (req.cookies);
+        res.send(name)
+    })
     .get('/check', (req, res) => {
-        const info = Object.entries(votes).map(([name, votesCount]) => `Votes on ${name}: ${votesCount}`).join('<br>');
-        res.send(info);
-    });
-
+        const { name } = (req.cookies);
+        res.send(
+            name === undefined ? 'Nie ma imienia' : 'Imię jest zapamiątane'
+        );
+    })
 module.exports = {
     cookieRouter,
 };
