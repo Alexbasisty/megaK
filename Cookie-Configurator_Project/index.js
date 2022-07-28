bba6d89ae4d3e3d9b4588808ea5d42a374aa416a
Alex Basystyi <alexbasisty@gmail.com>
2022-07-28 14:23:01 +0200

    handlebars helpers

--- Cookie-Configurator_Project/index.js
+++ Cookie-Configurator_Project/index.js
@@ -5,13 +5,17 @@ const hbs = require('express-handlebars');
 const { homeRouter } = require('./routes/home');
 const { configRouter } = require('./routes/configurator');
 const { orderRouter } = require('./routes/order');
+const { handlebarsHelpers } = require('./handlebars-helpers');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(cookieParser());
 
-app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
+app.engine('.hbs', hbs.engine({
+    extname: '.hbs',
+    helpers: handlebarsHelpers,
+}));
 app.set('view engine', '.hbs');
 
 app.use('/', homeRouter);
