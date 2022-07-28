2c1c45984766dc16370fdb9aa300cefba8c0b85b
Alex Basystyi <alexbasisty@gmail.com>
2022-07-28 14:55:23 +0200

    helper sum/price functions

--- Cookie-Configurator_Project/handlebars-helpers.js
+++ Cookie-Configurator_Project/handlebars-helpers.js
@@ -1,5 +1,17 @@
 const handlebarsHelpers = {
-    upper: str => str.toUpperCase(),
+    'find-price': (entries, selectedItem) => {
+        const found = entries.find(el => el[0] === selectedItem);
+
+        if (!found) {
+            throw new Error(`Cannot find price of ${selectedItem}`);
+        }
+
+        const [, price] = found;
+
+        return price;
+    },
+
+    'pricify': price => price.toFixed(2),
 };
 
 module.exports = {
--- Cookie-Configurator_Project/routes/home.js
+++ Cookie-Configurator_Project/routes/home.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies-data');
+const { handlebarsHelpers } = require('../handlebars-helpers');
 
 const homeRouter = express.Router();
 
 homeRouter
     .get('/', (req, res) => {
+        const sum = handlebarsHelpers['find-price'](Object.entries(COOKIE_BASES), 'light')
+            + ['coconut', 'honey'].reduce((prev, curr) => prev + handlebarsHelpers['find-price'](Object.entries(COOKIE_ADDONS), curr), 0);
+
         res.render('home/index', {
             cookie: {
                 base: 'light',
@@ -12,6 +16,7 @@ homeRouter
             },
             bases: Object.entries(COOKIE_BASES),
             addons: Object.entries(COOKIE_ADDONS),
+            sum,
         });
     });
 
