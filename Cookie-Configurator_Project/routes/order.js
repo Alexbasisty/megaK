5e4c1a15952fc9738c341fa83cc960e6f9704ae7
Alex Basystyi <alexbasisty@gmail.com>
2022-08-01 14:09:06 +0200

    finishing order

--- Cookie-Configurator_Project/routes/order.js
+++ Cookie-Configurator_Project/routes/order.js
@@ -27,6 +27,25 @@ orderRouter
             addons: Object.entries(COOKIE_ADDONS),
             sum,
         });
+    })
+    .get('/thanks', (req, res) => {
+        const { cookieBase } = req.cookies;
+
+        const addons = getAddonsFromRequest(req);
+
+        const sum = cookieBase
+            ?
+            (handlebarsHelpers['find-price'](Object.entries(COOKIE_BASES), cookieBase)
+                + addons.reduce((prev, curr) => prev + handlebarsHelpers['find-price'](Object.entries(COOKIE_ADDONS), curr), 0))
+            :
+            0;
+
+        res
+            .clearCookie('cookieBase')
+            .clearCookie('cookieAddons')
+            .render('order/thanks', {
+                sum,
+            });
     });
 
 module.exports = {
