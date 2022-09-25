160b8053656908b4e887066bd2076b09f4d26453
Alex Basystyi <alexbasisty@gmail.com>
2022-09-25 10:40:38 +0200

    redoing repository into active record

--- mini_projects/todo__fromSQLToMongo/records/todo.record.js
+++ mini_projects/todo__fromSQLToMongo/records/todo.record.js
@@ -17,6 +17,42 @@ class TodoRecord {
             throw new Error('Todo title should be at most 150 characters');
         }
     }
+    static async insert(record) {
+        TodoRepository._checkRecord(record);
+
+        const { insertedId } = await todos.insertOne(record);
+        record._id = insertedId;
+
+        return insertedId;
+    }
+
+    static async delete(record) {
+        TodoRepository._checkRecord(record);
+
+        await todos.deleteOne({
+            _id: record._id,
+        });
+    }
+
+    static async find(id) {
+        const todo = await todos.findOne({ _id: ObjectId(String(id)) });
+
+        return todo === null ? null : new TodoRecord(todo);
+    }
+
+    static async findAll() {
+        return todos.find().toArray();
+    }
+
+    static async update(record) {
+        TodoRepository._checkRecord(record);
+
+        await todos.replaceOne({
+            _id: record._id,
+        }, {
+            title: String(record.title),
+        });
+    }
 }
 
 module.exports = {
