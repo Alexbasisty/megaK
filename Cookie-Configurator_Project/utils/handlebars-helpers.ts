6f6957047ae4f9cf915bf3fc7c3acc79f6a085a0
Alex Basystyi <alexbasisty@gmail.com>
2022-10-29 15:59:28 +0200

    downloaded OOP version

--- Cookie-Configurator_Project/utils/handlebars-helpers.ts
+++ Cookie-Configurator_Project/utils/handlebars-helpers.ts
@@ -1,18 +1,19 @@
 const handlebarsHelpers = {
-    'find-price': (entries, selectedItem) => {
+    findPrice: (entries, selectedItem) => {
         const found = entries.find(el => el[0] === selectedItem);
 
         if (!found) {
-            throw new Error(`Cannot find price of ${selectedItem}`);
+            throw new Error(`Cannot find price of "${selectedItem}".`);
         }
 
         const [, price] = found;
-
         return price;
     },
 
-    'pricify': price => price.toFixed(2),
+    pricify: price => price.toFixed(2),
+
     isNotInArray: (array, element) => !array.includes(element),
+
     isInArray: (array, element) => array.includes(element),
 };
 
