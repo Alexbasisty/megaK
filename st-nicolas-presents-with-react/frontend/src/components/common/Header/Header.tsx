168db373b114f181d3a9f7bb533c3d9697cfe8ab
Alex Basystyi <alexbasisty@gmail.com>
2022-12-29 14:31:23 +0100

    added space between links

--- st-nicolas-presents-with-react/frontend/src/components/common/Header/Header.tsx
+++ st-nicolas-presents-with-react/frontend/src/components/common/Header/Header.tsx
@@ -9,6 +9,7 @@ const Header = () => {
         <>
           <h1>Santa App</h1>
           <NavLink to={'/gift'} style={linkColor}>Go to Gifts</NavLink>
+          <strong> </strong> 
           <NavLink to={'/child'} style={linkColor}>Go to Children</NavLink>
           <hr />
         </>
